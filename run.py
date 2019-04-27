ea7a2d2300d48e61ab3465a5e055058fcccd9ec6
JasonShadle <jason.shadle50@gmail.com>
2019-04-27 17:20:43 -0400

    Ready for live test

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ class MyClient(discord.Client):
         await welcome(member)
     # when a message is sent
     async def on_message(self, message):
-        print('Message in {0.channel.name} from {0.author}: {0.content}'.format(message))
+        # print('Message in {0.channel.name} from {0.author}: {0.content}'.format(message))
         # set some default variables to make it easier
         id = message.author.id
         content = message.content
@@ -44,18 +44,18 @@ class MyClient(discord.Client):
                 await message.channel.send(message.author.mention + ': You are not an admin')
         elif content[:7] == '!roles ':
             botMessage = ''
-            if channelName in ('role-request'):
+            if channelName in ('role-request', 'bot-setup-and-repair'):
                 if content[7:14] == 'toggle ':
                     if admin:
                         r = Roles(message)
                         roleName = content[14:].lower()
                         if r.toggle(roleName):
                             botMessage = await message.channel.send('{0}: Role `{1}` has been deleted'.format(message.author.mention, roleName))
-                            # await deleteTimedMessage(10, [message, botMessage])
+                            await deleteTimedMessage(10, [message, botMessage])
                             
                         else:
                             botMessage = await message.channel.send('{0}: Role `{1}` has been added'.format(message.author.mention, roleName))
-                    # await deleteTimedMessage(10, [message, botMessage])
+                    await deleteTimedMessage(10, [message, botMessage])
 
                 else:
                     r = Roles(message)
@@ -71,18 +71,19 @@ class MyClient(discord.Client):
                             await message.author.add_roles(r.getRole())
                             botMessage = await message.channel.send(message.author.mention + ': Role `'+ r.getRole().name + '` has been added.')
                 
-                    # await deleteTimedMessage(10, [message, botMessage])
+                    await deleteTimedMessage(10, [message, botMessage])
 
         elif content == '!roles':
-            embed = discord.Embed(color=0x006341, title='Self-Assigned Roles List:')
-            with open(config.rolesFile, 'r') as file:
-                for line in file:
-                    name = line
-                    value = '!roles ' + line
-                    embed.add_field(name=name, value=value, inline=False)
+            if channelName in ('role-request', 'bot-setup-and-repair'):
+                embed = discord.Embed(color=0x006341, title='Self-Assigned Roles List:')
+                with open(config.rolesFile, 'r') as file:
+                    for line in file:
+                        name = line
+                        value = '!roles ' + line
+                        embed.add_field(name=name, value=value, inline=False)
         
-            botMessage = await message.channel.send('{0}:'.format(message.author.mention), embed=embed)
-            # await deleteTimedMessage(60, [message, botMessage])
+                botMessage = await message.channel.send('{0}:'.format(message.author.mention), embed=embed)
+                await deleteTimedMessage(60, [message, botMessage])
         # elif content == '!logoff':
         #     if id in adminList:
         #         client.logoff()
